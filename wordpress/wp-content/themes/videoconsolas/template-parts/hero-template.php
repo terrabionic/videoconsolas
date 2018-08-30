78681aa3ae65746cd4117706798b53de654ad5e1
Luis Antonio Gomez <jimmylagp@gmail.com>
2018-08-29 22:48:25 -0500

    Building Hero section

--- wordpress/wp-content/themes/videoconsolas/template-parts/hero-template.php
+++ wordpress/wp-content/themes/videoconsolas/template-parts/hero-template.php
@@ -3,14 +3,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                Tagline / slogan
+                    <p class="hero__sentence">Tagline / <span class="hero__sub-sentence">slogan</span></p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-12">
                     <form role="search" method="get" action="<?= home_url('/'); ?>" class="hero__search-form">
                         <div class="hero__search-field">
-                            <input name="s" type="search" class="hero__search-input" placeholder="Consolas, videojuegos" value="<?= get_search_query() ?>" title="Consolas, videojuegos">
+                            <input name="s" type="search" class="hero__search-input" placeholder="Consolas, videojuegos" value="<?= get_search_query() ?>" title="Consolas, videojuegos" autocomplete="off">
                             <button type="submit" class="hero__search-btn">Buscar</button>
                         </div>
                     </form>
