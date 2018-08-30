5aa40f6814e6ee2de6127f5ec0639cd9219f4f79
Luis Antonio Gomez <jimmylagp@gmail.com>
2018-08-29 21:50:02 -0500

    Building sub menu

--- wordpress/wp-content/themes/videoconsolas/inc/filter-main-menu.php
+++ wordpress/wp-content/themes/videoconsolas/inc/filter-main-menu.php
@@ -9,6 +9,15 @@ function register_my_menu() {
 }
 add_action('init', 'register_my_menu');
 
+
+/**
+ * Add custom class for submenu
+ *
+ * @param [array] $classes
+ * @param [object] $args
+ * @param [object] $depth
+ * @return void
+ */
 function nav_menu_submenu_class($classes, $args, $depth) {
     if (in_array('sub-menu', $classes) && 'main-menu' === $args->theme_location) {
         $classes[] = 'header__nav-submenu';
@@ -17,15 +26,6 @@ function nav_menu_submenu_class($classes, $args, $depth) {
     return $classes;
 }
 add_filter('nav_menu_submenu_css_class', 'nav_menu_submenu_class', 10, 3);
-
-function nav_menu_item_filter($args, $item, $depth) {
-    if ($args->theme_location == 'main-menu' && in_array('menu-item-has-children', $item->classes)) {
-        $args->link_before = '';
-        $args->link_after  = '<span class="header__nav-link-arrow"></span>';
-    }
-    return $args;
-}
-add_filter('nav_menu_item_args', 'nav_menu_item_filter', 10, 3);
 
 /**
  * Add custom class to menu item
@@ -36,12 +36,18 @@ add_filter('nav_menu_item_args', 'nav_menu_item_filter', 10, 3);
  * @return void
  */
 function special_nav_class($classes, $item, $args) {
-    if (in_array('menu-item', $classes) && 'main-menu' === $args->theme_location) {
+    if (in_array('menu-item', $classes) && 'main-menu' === $args->theme_location && $item->menu_item_parent === '0') {
         $classes[] = 'header__nav-item list-inline-item';
+    } else {
+        $classes[] = 'header__nav-item list-unstyled';
     }
 
     if (in_array('menu-item-has-children', $classes) && 'main-menu' === $args->theme_location) {
         $classes[] = 'header__nav-item--has-children';
+    }
+
+    if (in_array('menu-item', $classes) && 'main-menu' === $args->theme_location && $item->menu_item_parent !== '0') {
+        $classes[] = 'header__nav-item--child';
     }
 
     return $classes;
@@ -57,10 +63,40 @@ add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 3);
  * @return void
  */
 function add_link_atts($atts, $item, $args) {
-    if ('main-menu' === $args->theme_location) {
+    if ('main-menu' === $args->theme_location && $item->menu_item_parent === '0') {
         $atts['class'] = "header__nav-link";
+    } else {
+        $atts['class'] = "header__nav-link header__nav-link--child";
     }
 
     return $atts;
 }
 add_filter('nav_menu_link_attributes', 'add_link_atts', 10, 3);
+
+/**
+ * Filtering menu link
+ *
+ * @param [object] $args
+ * @param [object] $item
+ * @param [object] $depth
+ * @return void
+ */
+function nav_menu_item_filter($args, $item, $depth) {
+    if ($args->theme_location == 'main-menu' && $item->menu_item_parent === '0') {
+        $args->link_before = '';
+        $args->link_after  = '';
+    }
+
+    if ($args->theme_location == 'main-menu' && in_array('menu-item-has-children', $item->classes) && $item->menu_item_parent === '0') {
+        $args->link_before = '';
+        $args->link_after  = '<span class="header__nav-link-arrow"></span>';
+    }
+
+    if ($args->theme_location == 'main-menu' && !in_array('menu-item-has-children', $item->classes) && $item->menu_item_parent !== '0') {
+        $args->link_before = '<span class="header__nav-link-dash">- </span>';
+        $args->link_after  = '';
+    }
+
+    return $args;
+}
+add_filter('nav_menu_item_args', 'nav_menu_item_filter', 10, 3);
