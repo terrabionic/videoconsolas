ae2f5b0d4f9596c46f6c18effba8f8cd1fdda074
Luis Antonio Gomez <jimmylagp@gmail.com>
2018-09-09 13:42:23 -0500

    Building sticky top bar

--- wordpress/wp-content/themes/videoconsolas/single.php
+++ wordpress/wp-content/themes/videoconsolas/single.php
@@ -18,4 +18,53 @@
     <?php endwhile; ?>
     <!-- End content -->
 
+    <section class="nav-posts">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="nav-posts__social">
+                        <ul class="nav-posts__list">
+                            <li class="nav-posts__btn">
+                                <a href="#" class="nav-posts__btn-link nav-posts__btn-link--fb">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li class="nav-posts__btn">
+                                <a href="#" class="nav-posts__btn-link nav-posts__btn-link--tt">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li class="nav-posts__btn">
+                                <a href="#" class="nav-posts__btn-link nav-posts__btn-link--gplus">
+                                    <i class="fab fa-google-plus-g"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="nav-posts__buttons">
+                        <ul class="nav-posts__list nav-posts__list--right">
+                            <li class="nav-posts__btn">
+                                <a href="#" class="nav-posts__btn-link">
+                                    <img src="<?= get_template_directory_uri(); ?>/dist/images/arrow-previous.svg" alt="" class="nav-posts__btn-icon">
+                                </a>
+                            </li>
+                            <li class="nav-posts__btn">
+                                <a href="#" class="nav-posts__btn-link">
+                                    <img src="<?= get_template_directory_uri(); ?>/dist/images/arrow-random.svg" alt="" class="nav-posts__btn-icon">
+                                </a>
+                            </li>
+                            <li class="nav-posts__btn">
+                                <a href="#" class="nav-posts__btn-link">
+                                    <img src="<?= get_template_directory_uri(); ?>/dist/images/arrow-next.svg" alt="" class="nav-posts__btn-icon">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 <?php get_footer(); ?>
