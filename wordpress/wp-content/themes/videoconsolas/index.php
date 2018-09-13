e4c5db16eacd89ebf03ce2b2e74b4bca2785a3b4
Luis Antonio Gomez <jimmylagp@gmail.com>
2018-09-12 22:46:47 -0500

    Add switcher layout

--- wordpress/wp-content/themes/videoconsolas/index.php
+++ wordpress/wp-content/themes/videoconsolas/index.php
@@ -6,6 +6,19 @@
     <section class="posts-list">
         <div class="container">
             <div class="row">
+                <div class="col-lg-8">
+                    <div class="posts-list__switch-buttons">
+                        <button class="posts-list__switch-layout posts-list__switch-layout--active posts-list__switch-layout--grid">
+                            <svg version="1.1" id="grid-layout" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 29.1 28.9" style="enable-background:new 0 0 29.1 28.9;" xml:space="preserve"><path class="st0" d="M10.8,0H1c-0.6,0-1,0.5-1,1v9.8c0,0.6,0.5,1,1,1h9.8c0.6,0,1-0.5,1-1V1.1C11.8,0.5,11.4,0,10.8,0z M9.8,9.8 H2.1V2.1h7.7V9.8z"></path><path class="st0" d="M28,0h-9.8c-0.6,0-1,0.5-1,1v9.8c0,0.6,0.5,1,1,1H28c0.6,0,1-0.5,1-1V1.1C29,0.5,28.6,0,28,0z M27,9.8h-7.7 V2.1H27V9.8z"></path><path class="st0" d="M10.8,17H1c-0.6,0-1,0.5-1,1v9.8c0,0.6,0.5,1,1,1h9.8c0.6,0,1-0.5,1-1V18C11.8,17.4,11.4,17,10.8,17z M9.8,26.7H2.1V19h7.7V26.7z"></path><path class="st0" d="M28,17h-9.8c-0.6,0-1,0.5-1,1v9.8c0,0.6,0.5,1,1,1H28c0.6,0,1-0.5,1-1V18C29,17.4,28.6,17,28,17z M27,26.7 h-7.7V19H27V26.7z"></path></svg>
+                        </button>
+                        <button class="posts-list__switch-layout posts-list__switch-layout--list">
+                            <svg version="1.1" id="list-layout" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 27.2 23.8" style="enable-background:new 0 0 27.2 23.8;" xml:space="preserve"><g> <circle class="st0" cx="1.7" cy="1.6" r="1.6"></circle><path class="st0" d="M7.6,2.9h18.2c0.7,0,1.3-0.6,1.3-1.3s-0.6-1.3-1.3-1.3H7.6c-0.7,0-1.3,0.6-1.3,1.3S6.9,2.9,7.6,2.9z"></path><circle class="st0" cx="1.7" cy="11.9" r="1.6"></circle><path class="st0" d="M25.8,10.6H7.6c-0.7,0-1.3,0.6-1.3,1.3s0.6,1.3,1.3,1.3h18.2c0.7,0,1.3-0.6,1.3-1.3S26.5,10.6,25.8,10.6z"></path><circle class="st0" cx="1.7" cy="22.2" r="1.6"></circle><path class="st0" d="M25.8,20.9H7.6c-0.7,0-1.3,0.6-1.3,1.3c0,0.7,0.6,1.3,1.3,1.3h18.2c0.7,0,1.3-0.6,1.3-1.3 C27.1,21.5,26.5,20.9,25.8,20.9z"></path></g></svg>
+                        </button>
+                    </div>
+                    <hr class="posts-list__switch-divider">
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-lg-4 order-last">
                     dsadasdas
                 </div>
@@ -13,7 +26,7 @@
                     <?php get_template_part('template-parts/banner-content-section'); ?>
                     <div class="posts-list__container">
                     <?php while (have_posts()) : the_post(); ?>
-                        <article class="posts-list__entry posts-list__entry--large">
+                        <article class="posts-list__entry">
                             <a href="<?= esc_url( get_permalink() ); ?>" class="posts-list__entry-image">
                                 <?php
                                 if(has_post_thumbnail()) {
--- wordpress/wp-content/themes/videoconsolas/src/js/main.js
+++ wordpress/wp-content/themes/videoconsolas/src/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    /* Top bar when scrolling */
     if ($('.nav-posts').length > 0) {
         $(window).scroll(function () {
             var navTopBar = $('.nav-posts'),
@@ -9,6 +10,24 @@ $(document).ready(function () {
                 navTopBar[0].classList.add("nav-posts--sticky");
             } else {
                 navTopBar[0].classList.remove("nav-posts--sticky");
+            }
+        })
+    }
+
+    /* Switch layout posts */
+    if ($('.posts-list__switch-layout').length) {
+        $('.posts-list__switch-layout').on('click touch', function () {
+            if (!$(this).hasClass('posts-list__switch-layout--active')) {
+                $('.posts-list__switch-layout').removeClass('posts-list__switch-layout--active')
+                $(this).addClass('posts-list__switch-layout--active')
+
+                if ($('.posts-list__switch-layout--grid').hasClass('posts-list__switch-layout--active')) {
+                    $('.posts-list__entry').removeClass('posts-list__entry--large')
+                }
+
+                if ($('.posts-list__switch-layout--list').hasClass('posts-list__switch-layout--active')) {
+                    $('.posts-list__entry').addClass('posts-list__entry--large')
+                }
             }
         })
     }
