f3a3e1e9c3914233cf28cf43184fc059757d4a21
Luis Antonio Gomez <jimmylagp@gmail.com>
2018-10-08 20:30:10 -0500

    Fixing issues

--- wordpress/wp-content/themes/videoconsolas/template-parts/share-buttons-section.php
+++ wordpress/wp-content/themes/videoconsolas/template-parts/share-buttons-section.php
@@ -1,17 +1,17 @@
 <div class="content-share share-buttons">
     <ul class="share-buttons__list">
         <li class="share-buttons__item share-buttons__item--fb">
-            <a href="" class="share-buttons__item-link">
+            <a href="https://www.facebook.com/sharer/sharer.php?u=<?= esc_url(get_the_permalink($post->ID)); ?>" target="_blank" class="share-buttons__item-link">
                 <i class="fab fa-facebook-f"></i>
             </a>
         </li>
         <li class="share-buttons__item share-buttons__item--tt">
-            <a href="" class="share-buttons__item-link">
+            <a href="https://twitter.com/home?status=<?= urlencode(get_the_title($post->ID))." - ".esc_url(get_the_permalink($post->ID)); ?>" target="_blank" class="share-buttons__item-link">
                 <i class="fab fa-twitter"></i>
             </a>
         </li>
         <li class="share-buttons__item share-buttons__item--gplus">
-            <a href="" class="share-buttons__item-link">
+            <a href="https://plus.google.com/share?url=<?= esc_url(get_the_permalink($post->ID)); ?>" target="_blank" class="share-buttons__item-link">
                 <i class="fab fa-google-plus-g"></i>
             </a>
         </li>
